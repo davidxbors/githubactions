b77850bfdb97b78e55785262e2526b8e55da6f84
David Bors <daviddvd267@gmail.com>
2022-07-31 11:10:56 +0300

    Update rule 104 and disable rule 010

--- .github/workflows/rules/md104.js
+++ .github/workflows/rules/md104.js
@@ -1,31 +1,35 @@
 "use strict";
 
-/*
-ignore lists!
-ignore headers!
-*/
-
 module.exports = {
-  "names": [ "MD104", "one line per sentence" ],
-  "description": "one line (and only one line) per sentence",
-  "tags": [ "sentences" ],
-  "function": function rule(params, onError) {
+  names: ["MD104", "one line per sentence"],
+  description: "one line (and only one line) per sentence",
+  tags: ["sentences"],
+  function: function rule(params, onError) {
     for (const inline of params.tokens.filter(function filterToken(token) {
       return token.type === "inline";
     })) {
-	var actual_lines = inline.content.split("\n");
-	actual_lines.forEach( (line, index, arr) => {
-	    /* check how many sentence endings are per line */
-	    var dots = line.split(".").length - 1;
-	    var question_marks = line.split("?").length - 1;
-	    var exclamation_marks = line.split("!").length - 1;
-	    /* there should be only one sentence ending per line! */
-	    if (dots + question_marks + exclamation_marks > 1) {
-		    onError({
-			    "lineNumber": inline.lineNumber + index,
-			    "detail": "Expected one sentence per line. Multiple end of sentence punctuation signs found on one line!"
-		    });
-	    }});
+      var actual_lines = inline.content.split("\n");
+      actual_lines.forEach((line, index, arr) => {
+		let outside = true;
+		let count = 0;
+		Array.from(line).forEach((char) => {
+			if ((char == "." || char == "?" || char == "!" || char == ";" || char == ":") && outside) {
+				count++;
+			}
+			if (char == "`") outside = !outside;
+			if (char == "[") outside = false;
+			if (char == "(") outside = false;
+			if (char == "]") outside = true;
+			if (char == ")") outside = true;
+		});
+        if (count > 1) {
+          onError({
+            lineNumber: inline.lineNumber + index,
+            detail:
+              "Expected one sentence per line. Multiple end of sentence punctuation signs found on one line!",
+          });
+        }
+      });
     }
-  }
+  },
 };
